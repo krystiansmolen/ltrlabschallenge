d35f09c9768ca6132800b785e33a474a6f0d0070
Krystian Smoleń <krystian.smolen@icloud.com>
2025-05-04 11:33:15 +0200

    added types for schemas

--- lib/schema/order.ex
+++ lib/schema/order.ex
@@ -1,3 +1,10 @@
 defmodule Schema.Order do
+  @type t :: %__MODULE__{
+          net_total: Decimal.t() | nil,
+          tax: Decimal.t() | nil,
+          total: Decimal.t() | nil,
+          items: [OrderItem.t()] | nil
+        }
+
   defstruct [:net_total, :tax, :total, :items]
 end
--- lib/schema/order_item.ex
+++ lib/schema/order_item.ex
@@ -1,3 +1,10 @@
 defmodule Schema.OrderItem do
+  @type t :: %__MODULE__{
+          net_price: Decimal.t(),
+          quantity: integer(),
+          net_total: Decimal.t() | nil,
+          total: Decimal.t() | nil
+        }
+
   defstruct [:net_price, :quantity, :net_total, :total]
 end
