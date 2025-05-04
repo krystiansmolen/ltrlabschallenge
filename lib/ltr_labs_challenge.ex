c25ca65fcd7cd4571f693efa7b4d5c739f1c3c99
Krystian Smoleń <krystian.smolen@icloud.com>
2025-05-04 10:41:17 +0200

    first commit

---
+++ lib/ltr_labs_challenge.ex
@@ -0,0 +1,18 @@
+defmodule LtrLabsChallenge do
+  @moduledoc """
+  Documentation for `LtrLabsChallenge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LtrLabsChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ltr_labs_challenge_test.exs
@@ -0,0 +1,8 @@
+defmodule LtrLabsChallengeTest do
+  use ExUnit.Case
+  doctest LtrLabsChallenge
+
+  test "greets the world" do
+    assert LtrLabsChallenge.hello() == :world
+  end
+end
